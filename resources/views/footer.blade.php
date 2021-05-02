5ca75f893ca039ff25009ffbbea4fab97a49d3bd
George Diepiriye <georgediepiriye4u@gmail.com>
2021-05-02 10:31:36 +0100

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <h2>Panel Footer</h2>
+    <div class="panel panel-default">
+      <div class="panel-heading">Panel Heading</div>
+      <div class="panel-body">Panel Content</div>
+      <div class="panel-footer">Panel Footer</div>
+    </div>
+  </div>
